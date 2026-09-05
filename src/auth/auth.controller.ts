d63d53f41f6757d86bdf0bc119b0b319b77a395c
natiDogg <natnael.wondmu@gmail.com>
2026-09-05 08:01:54 -0700

    created my auth controller logics in auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller,Post,Get,Body,BadRequestException } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterDto } from './dtos/registerDto';
+import { LoginDto } from './dtos/loginDto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+      constructor(private readonly authService:AuthService){}
+
+
+      @Post('register')
+      async registerUser(@Body() registerDetails:RegisterDto){
+            return await this.authService.register(registerDetails)
+      }
+
+      @Post("login")
+      async loginUser(@Body() loginDetails: LoginDto){
+           return await this.authService.login(loginDetails)
+      }
+
+      @Post("refresh")
+      async refreshToken(@Body('refreshToken') refreshToken: string){
+         if(!refreshToken){
+            throw new BadRequestException("Token is Required")
+         }
+         return await this.authService.refreshToken( refreshToken)
+      }
+      
+}
