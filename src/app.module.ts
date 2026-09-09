62b3fd8c0ba7fc0e675c42ae2b11fdbd8ac67ef6
natiDogg <natnael.wondmu@gmail.com>
2026-09-09 04:51:16 -0700

    created service module and controller for category

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,12 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { AuthCommonModule } from './auth-shared-module/auth-shared-module.module';
 import { ArticleModule } from './article/article.module';
+import { CategoryModule } from './category/category.module';
 @Module({
   imports: [PrismaModule, ConfigModule.forRoot({
      isGlobal: true,
      validate
-  }), AuthModule, UserModule, AuthCommonModule, ArticleModule],
+  }), AuthModule, UserModule, AuthCommonModule, ArticleModule, CategoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
